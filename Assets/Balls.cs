e7e02c36bb43532ad5cc7babebc8884a2a449834
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-09-22 00:34:42 -0400

    Add Log Message when raise, lower and renew pins

---
+++ Assets/Balls.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balls : MonoBehaviour {
+	public Camera cmr;
+	public Rigidbody rb;
+
+	public float offset;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		offset = cmr.transform.position.z - transform.position.z;
+	}
+
+	public void Launch (Vector3 speed)
+	{
+		//if (Input.GetMouseButtonDown (0)) {
+			rb.velocity = speed;
+		//}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Launch ();
+		if (cmr.transform.position.z < 800) {
+			cmr.transform.position = new Vector3 (cmr.transform.position.x, cmr.transform.position.y, offset + transform.position.z);
+		}
+	}
+}
---
+++ Assets/PinSetter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RaisePins()
+	{
+		//Raise Standing pins
+		Debug.Log ("Raise Pins");
+
+	}
+
+	public void LowerPins()
+	{
+		Debug.Log ("Lower Pins");
+	}
+
+
+	public void RenewPins()
+	{
+		Debug.Log ("Renew Pins");
+	}
+
+}
