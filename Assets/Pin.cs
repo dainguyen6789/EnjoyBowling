acc5613d22ef73fbec062f89754440e5e6c88e8b
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-09-23 08:55:58 -0400

    Put the Raise() method in Pins.cs The RaisePin() method which is in the Swiper.cs will call Raise() method

--- Assets/Pin.cs
+++ Assets/Pin.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-public class Pins : MonoBehaviour {
+
+public class Pin : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//rigidbody.useGravity = false;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
-
 }
--- Assets/PinSetter.cs
+++ Assets/PinSetter.cs
@@ -4,31 +4,51 @@ using System.Collections;
 public class PinSetter : MonoBehaviour {
 
 	// Use this for initialization
+	Rigidbody rg;
+	float distancetoRaise=40.0f;
 	void Start () {
+		rg = GetComponent<Rigidbody> ();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//rigidBody.useGravity = true;
 	}
-
 	public void RaisePins()
 	{
 		//Raise Standing pins
 		Debug.Log ("Raise Pins");
-
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+		{
+			
+			//Rigidbody rb=GetComponent<Rigidbody>();
+			//rb.useGravity=false;
+			pin.transform.Translate(new Vector3(0,distancetoRaise,0),Space.World);
+			//pin.GetComponent<Rigidbody>().useGravity=false;
+			
+			
+			
+		}
+		
 	}
-
-	public void LowerPins()
-	{
-		Debug.Log ("Lower Pins");
+	
+	public void Lower(){
+		
+		Debug.Log("Lower");
+		
+		//transform.position = startPos;
+		
+		//ridgidbody.useGravity = true;
+		
 	}
-
-
+	
+	
 	public void RenewPins()
 	{
 		Debug.Log ("Renew Pins");
 	}
+
+
 
 }
